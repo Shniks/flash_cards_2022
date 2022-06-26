c4e75d4ce10f3cb4d2f80991771b0845266c94f5
Nikhil Shahi <shniks@gmail.com>
2022-06-26 10:21:42 -0700

    created tests and method to output cards per category.

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,13 +1,21 @@
+require 'pry'
+
 class Deck
 
   attr_reader :cards
 
-  def initialize(cards = 0)
+  def initialize(cards = [])
     @cards = cards
   end
 
   def count
     @cards.count
+  end
+
+  def cards_in_category(category)
+    @cards.find_all do |card|
+      card.category == category
+    end
   end
 
 end
