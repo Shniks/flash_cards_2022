35179e9bbb270c989a72f4317e86539c34ab5914
Nikhil Shahi <shniks@gmail.com>
2022-06-26 08:58:57 -0700

    created tests for Turn class to check existence and attributes.

---
+++ test/turn_test.rb
@@ -0,0 +1,28 @@
+require 'simplecov'
+SimpleCov.start
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry'
+require './lib/turn.rb'
+require './lib/card.rb'
+
+class TurnTest < Minitest::Test
+
+  def test_that_it_exists
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    assert_instance_of Turn, turn
+  end
+
+  def test_if_it_has_attributes
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+
+    assert_equal "Juneau", turn.guess
+    assert_instance_of Card, turn.card
+  end
+
+
+
+end
