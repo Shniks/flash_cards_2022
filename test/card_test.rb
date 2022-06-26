edb01bf18566de3dc38baa2f357fe8b058631dc0
Nikhil Shahi <shniks@gmail.com>
2022-06-26 08:29:47 -0700

    modified Card class path.

--- test/card_test.rb
+++ test/card_test.rb
@@ -3,9 +3,15 @@ SimpleCov.start
 require 'minitest/autorun'
 require 'minitest/pride'
 require 'pry'
-require '../lib/card.rb'
+require './lib/card.rb'
 
 class CardTest < Minitest::Test
+
+  def test_that_it_exists
+    card = Card.new
+
+    assert_instance_of Card, card
+  end
 
 
 
