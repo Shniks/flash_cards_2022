f1bc4757eb043ea31f46dec1b4af999c14d37825
Nikhil Shahi <shniks@gmail.com>
2022-06-26 08:42:02 -0700

    created tests and code to check if class exists and it has attributes.

--- test/card_test.rb
+++ test/card_test.rb
@@ -8,10 +8,20 @@ require './lib/card.rb'
 class CardTest < Minitest::Test
 
   def test_that_it_exists
-    card = Card.new
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 
     assert_instance_of Card, card
   end
+
+  def test_it_has_attributes
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+
+    assert_equal "What is the capital of Alaska?", card.question
+    assert_equal "Juneau", card.answer
+    assert_equal :Geography, card.category
+  end 
+
+
 
 
 
