22286f09b33d5d814c54dcf2f7690167079ec76f
Nikhil Shahi <shniks@gmail.com>
2022-06-27 09:53:30 -0700

    created test and methods for an incorrect guess.

--- test/round_test.rb
+++ test/round_test.rb
@@ -74,8 +74,23 @@ class RoundTest < Minitest::Test
     round = Round.new(deck)
     round.take_turn("Juneau")
     round.number_correct
-    binding.pry
+
     assert_equal card_2, round.current_card
+  end
+
+  def test_it_checks_for_incorrect_card
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+    round.take_turn("Juneau")
+    round.number_correct
+    round.take_turn("Venus")
+
+    assert_equal 2, round.turns.count
+    assert_equal "Incorrect.", round.turns.last.feedback
+    assert_equal 1, round.number_correct 
   end
 
 
