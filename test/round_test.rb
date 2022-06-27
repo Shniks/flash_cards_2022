1dc858c9efd9c40c474c37baeae7832d2724d3a9
Nikhil Shahi <shniks@gmail.com>
2022-06-27 11:23:08 -0700

    created test to check for third current card. Completed iteration 2.

--- test/round_test.rb
+++ test/round_test.rb
@@ -139,4 +139,17 @@ class RoundTest < Minitest::Test
     assert_equal 100.0, round.percent_correct_by_category(:Geography)
   end
 
+  def test_it_checks_for_third_current_card
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+    round.take_turn("Juneau")
+    round.number_correct
+    round.take_turn("Venus")
+
+    assert_equal card_3, round.current_card
+  end
+
 end
