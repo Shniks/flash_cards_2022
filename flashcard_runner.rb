a7339fadf99376d93b0b6c38343267d7d9cd49bc
Nikhil Shahi <shniks@gmail.com>
2022-06-27 20:16:55 -0700

    refactored runner and the Round class. Moved runner code to the end of the Round class.

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -1,6 +1,9 @@
-require './lib/card'
 require './lib/deck'
 require './lib/round'
-require './lib/turn'
 require './lib/card_generator.rb'
 require 'pry'
+
+create_cards = CardGenerator.new('./cards.txt').cards
+deck = Deck.new(create_cards)
+round = Round.new(deck)
+round.start
--- lib/round.rb
+++ lib/round.rb
@@ -47,4 +47,30 @@ class Round
     (@correct_by_category[category].to_f / @number_by_category[category])*100.round(0)
   end
 
+#========= For runner file =====================
+
+  def start
+    puts "Welcome to Flashcards\n_____________________\n\n"
+    puts "You are playing with #{deck.count} cards.\n\n"
+    question_and_guess
+  end
+
+  def question_and_guess
+    deck.cards.each do |card|
+      question_and_feedback
+    end
+  end
+
+  def question_and_feedback
+    puts "This is card number #{card_count+ 1} out of #{deck.count} cards.\n"
+    puts "Question: #{current_card.question}\n"
+    guess = gets.chomp
+    take_turn(guess)
+    puts turns.last.feedback + "\n\n"
+  end
+
+
+
+
+
 end
